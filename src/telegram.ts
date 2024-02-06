482e9c1d10704c4bd13275dcd484d9fa5385b4ec
shayandrg <shayan.derogarian@gmail.com>
2024-02-07 03:20:28 +0330

    feat: telegram downlaod and runnig quee

--- src/telegram.ts
+++ src/telegram.ts
@@ -1,34 +1,35 @@
 import { Telegraf } from 'telegraf'
 import { message } from 'telegraf/filters'
-const Downloader = require("nodejs-file-downloader");
+const Downloader = require('nodejs-file-downloader');
 
 export default class Telegram {
+    app = null
     bot = null
 
     constructor(app) {
+        this.app = app
         this.bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
         this.boot()
     }
 
     boot() {
         this.bot.on(message('video'), async (ctx) => {
-            const fileLink = await ctx.telegram.getFileLink(ctx.message.video.file_id)
-            const downloader = new Downloader({ url: fileLink.href, directory: './downloads' })
-            let filePath = null
+            const fileLink = await ctx.telegram.getFileLink(ctx.message.video.file_id)          
+
+            const downloader = new Downloader({ url: fileLink.href, directory: './downloads', fileName: `${ctx.message.video.file_unique_id}.mp4` })
             await downloader.download()
-            .then(res => {
-                filePath = res.filePath
-                        // await this.app.db.discordUploadQuee.create({
-                        //     data: {
-                        //         fileName: 'file_0.mp4',
-                        //         fileSize: 2653785
-                        //     }
-                        // })
-                console.log(res);
-            })
             .catch(err => {
                 console.log(err);
             })
+
+            await this.app.db.discordUploadQuee.create({
+                data: {
+                    fileName: ctx.message.video.file_unique_id,
+                    fileSize: ctx.message.video.file_size
+                }
+            })
+
+            this.app.discord.checkQuee()
         })
     }
 }
