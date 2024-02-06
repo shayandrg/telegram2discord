c1690ab860b9d49714caadcb1dc5e86492276760
shayandrg <shayan.derogarian@gmail.com>
2024-02-07 03:19:58 +0330

    feat: add axios with socks5 proxy http agent

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 process.on('uncaughtException', err => console.error('Uncaught Exception:', err))
 
 import 'dotenv/config'
+import axios from 'axios'
 import { PrismaClient } from '@prisma/client'
 import Telegram from './telegram'
 import Discord from './discord'
+import { SocksProxyAgent } from 'socks-proxy-agent'
+
+const httpsAgent = new SocksProxyAgent(`socks5://127.0.0.1:${process.env.PROXY_PORT}`)
 
 const app = {
+    axios: axios.create({ httpsAgent, httpAgent: httpsAgent }),
     db: new PrismaClient(),
     telegram: null,
     discord: null
